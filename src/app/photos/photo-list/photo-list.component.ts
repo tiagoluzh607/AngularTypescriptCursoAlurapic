438d71e6bd289fdcf3b3291aff1c1b7e1d021a75
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-10-26 18:59:57 -0200

    Passando parâmetros na rota e pegando no component

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -11,11 +12,17 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   
-  constructor(private photoService: PhotoService){}
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+  ){}
   
   ngOnInit(): void {
+
+    const userName: string = this.activatedRoute.snapshot.params.userName; //pega o parametro da rota 
+
     this.photoService
-    .listFromUser('flavio')
+    .listFromUser(userName)
     .subscribe(photos => this.photos = photos);
   }
 
