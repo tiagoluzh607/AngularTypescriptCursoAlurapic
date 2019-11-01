3bd775bc1c4d6c005e9f68ef4dba6d0638a1a7f9
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-11-01 20:56:27 -0200

    Criando component Card

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PhotoModule } from './photo/photo.module';
 import { PhotoFormModule } from './photo-form/photo-form-module';
 import { PhotoListModule } from './photo-list/photo-list.module';
+import { CardModule } from '../shared/card/card.module';
 
 @NgModule({
     imports: [
