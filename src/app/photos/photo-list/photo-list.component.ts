f094683c3bd48aa049da241c1b29f93195bffc09
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-10-31 23:44:30 -0200

    Evitando multiplas requisições com padrao debounce, utilizando RxJS

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Photo } from '../photo/photo';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { Photo } from '../photo/photo';
+import { debounceTime } from 'rxjs/operators'
 
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
+  
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+
 
   constructor(
     private activatedRoute: ActivatedRoute
@@ -18,6 +23,17 @@ export class PhotoListComponent implements OnInit {
   
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos; // conseguimos pegar os parâmetros passados na rota
+    this.debounce
+      .pipe(debounceTime(300)) //vai aguardar 300 miliseg se vier mais chamadas dentro desse periodo ele ignora
+      .subscribe(filter => this.filter = filter);
+    //this.debounce.next('f'); emite um valor
+    //this.debounce.subscribe(value => alert(value)); escuta o valor emitido
   }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe(); //libera a memória do escutador
+  }
+
+  
 
 }
