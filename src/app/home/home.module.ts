ce8ffee750ca61078bd4cbb915a1749d8183a85b
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-11-12 23:20:14 -0200

    transformando menssagens de erro em components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import {ReactiveFormsModule} from '@angular/forms'
 
 import { SignInComponent } from './signin/signin.component';
 import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/vmessage/vmessage.module';
 
 
 @NgModule({
     declarations: [SignInComponent],
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        VMessageModule
     ]
 })
 export class HomeModule{
