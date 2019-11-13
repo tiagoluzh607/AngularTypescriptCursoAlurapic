ca977333339ae2936790ce1b90fcedb3c47b43e1
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-11-12 23:04:42 -0200

    Validacao de Formulario Login com validacao Angular

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signin.component.html'
 })
-export class SignInComponent{}
+export class SignInComponent implements OnInit{
+
+
+    loginForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder){
+
+    }
+
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required], //deve ser o nome do input (?1, ?2, ?3) ?1- valor padrao do input
+            password: ['', Validators.required]
+        })
+    }
+}
