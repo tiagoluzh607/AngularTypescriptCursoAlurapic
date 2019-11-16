2b105e48227bedebb5e231f0512d1a4dc51bc43c
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-11-16 14:21:59 -0200

    pegando novo Usuario do form e cadastrando no sistema

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { lowerCaseValidator } from '../../shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -12,7 +15,9 @@ export class SignUpComponent implements OnInit {
 
     constructor(
         private formBuilder: FormBuilder,
-        private userNotTakenValidatorService: UserNotTakenValidatorService
+        private userNotTakenValidatorService: UserNotTakenValidatorService,
+        private signUpService: SignUpService,
+        private router: Router
     ) {
 
     }
@@ -44,6 +49,16 @@ export class SignUpComponent implements OnInit {
                 Validators.maxLength(18)
             ]],
         });
+    }
+
+    signup(){
+        const newUser = this.signupForm.getRawValue() as NewUser;
+        this.signUpService
+            .signup(newUser)
+            .subscribe(()=>
+                this.router.navigate(['']),
+                err => console.log(err)
+            )
     }
 
 }
