b4dd2bc0f7151de7f756d77c061844e59a51128f
tiagoluzh607 <tiagoluz.h607@gmail.com>
2020-03-02 22:08:40 -0300

    Criando Diretiva para exibir elementos somente se o usuario estiver logado

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -34,7 +34,14 @@ const routes: Routes = [
         path: 'p/:photoId', 
         component: PhotoDetailsComponent,
     },
-    { path: '**', component: NotFoundComponent}
+    { 
+        path: 'not-found', 
+        component: NotFoundComponent
+    },
+    {   
+        path: '**', 
+        redirectTo: 'not-found'
+    }
 ];
 
 @NgModule({
--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -27,6 +27,10 @@ export class PhotoDetailsComponent implements OnInit{
        this.photoId = this.route.snapshot.params.photoId;
        
        this.photo$ = this.photoService.findById(this.photoId);
+       this.photo$.subscribe(()=>{}, err =>{
+           console.log(err);
+           this.router.navigate(['not-found'])
+       })
     }
 
     remove(){
